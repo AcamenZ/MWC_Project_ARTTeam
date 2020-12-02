ff027ee9f6bc5cb0e2d4459278257b6169d8b2d5
AcamenZ <a.camenzind.1991@gmail.com>
2020-12-02 22:15:52 +0100

    Created TABLE physical activity with their value, TABLE ranking under work

--- app/src/main/java/com/example/stepapp/StepAppOpenHelper.java
+++ app/src/main/java/com/example/stepapp/StepAppOpenHelper.java
@@ -25,11 +25,50 @@ public class StepAppOpenHelper extends SQLiteOpenHelper {
     public static final String KEY_HOUR = "hour";
     public static final String KEY_DAY = "day";
 
+    public static final String TABLE_NAME_PA = "Physical_activities";
+    public static final String ID_PA = "id";
+    public static final String NAME = "name";
+    public static final String MIN_TEMP = "min_temp";
+    public static final String MAX_TEMP = "max_temp";
+    public static final String O_MIN_TEMP = "optimal_min_temp";
+    public static final String O_MAX_TEMP = "optimal_max_temp";
+    public static final String O_MIN_VIS = "optimal_min_visibility";
+    public static final String MIN_WIND = "min_wind_speed";
+    public static final String MAX_WIND = "max_wind_speed";
+    public static final String O_MIN_WIND_SPEED = "optimal_min_wind_speed";
+    public static final String O_MAX_WIND_SPEED = "optimal_max_wind_speed";
+    public static final String MIN_RAIN = "min_rain_1h";
+    public static final String MAX_RAIN = "max_rain_1h";
+    public static final String O_MIN_RAIN = "optimal_min_rain_1h";
+    public static final String O_MAX_RAIN = "optimal_max_rain_1h";
+    public static final String MIN_SNOW = "min_snow_1h";
+    public static final String MAX_SNOW = "max_snow_1h";
+    public static final String O_MIN_SNOW = "optimal_min_snow_1h";
+    public static final String O_MAX_SNOW = "optimal_max_snow_1h";
+    public static final String O_DAYTIME = "optimal_daytime";
+
+    public static final String TABLE_NAME_PAR = "Physical_activities_rankings";
+    public static final String ID_PAR ="id_r";
+    public static final String PA_id="physical_activity_id";
+    public static final String DATE="datetime";
+    public static final String RATING="rating";
+    public static final String REASON="reason";
+    public static final String DATE_PUSH="push_out_time";
+    public static final String SENT="sent";
+
 
     // Default SQL for creating a table in a database
     public static final String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_NAME + " (" +
             KEY_ID + " INTEGER PRIMARY KEY, " + KEY_DAY + " TEXT, " + KEY_HOUR + " TEXT, "
             + KEY_TIMESTAMP + " TEXT);";
+    public static final String CREATE_TABLE_SQL2 ="CREATE TABLE " + TABLE_NAME_PA +"(" +
+            ID_PA + "INTEGER PRIMARY KEY," + NAME  + "VARCHAR," + MIN_TEMP + "FLOAT," + MAX_TEMP +"FLOAT," + O_MIN_TEMP +"FLOAT," +
+            O_MAX_TEMP +"FLOAT," + O_MIN_VIS + "FLOAT," + MIN_WIND + "FLOAT," + MAX_WIND + "FLOAT," + O_MIN_WIND_SPEED + "FLOAT," +
+            O_MAX_WIND_SPEED + "FLOAT," + MIN_RAIN + "FLOAT," + MAX_RAIN + "FLOAT," + O_MIN_RAIN + "FLOAT," + O_MAX_RAIN + "FLOAT," +
+            MIN_SNOW + "FLOAT," + MAX_SNOW + "FLOAT," + O_MIN_SNOW + "FLOAT," + O_MAX_SNOW + "FLOAT," + O_DAYTIME + "BOOLEAN);";
+    public static final String CREATE_TABLE_SQL3 = "CREATE TABLE " + TABLE_NAME_PAR +" (" +
+            ID_PAR + "INTEGER PRIMARY KEY," + PA_id  + "INT," + DATE + "DATETIME," + RATING +"INT," + REASON +"VARCHAR," +
+            DATE_PUSH +"DATETIME," + SENT + "BOOLEAN);";
 
 
     // The constructor
@@ -42,6 +81,14 @@ public class StepAppOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_SQL);
+        db.execSQL(CREATE_TABLE_SQL2);
+        db.execSQL(CREATE_TABLE_SQL3);
+        db.execSQL("INSERT INTO " + TABLE_NAME_PA+ "(id, name, min_temp, max_temp, optimal_min_temp, optimal_max_temp, optimal_min_visibility, min_wind_speed, max_wind_speed, optimal_min_wind, optimal_max_wind, min_rain_1h, max_rain_1h, optimal_min_rain_1h, optimal_max_rain_1h, min_snow_1h, max_snow_1h, optimal_min_snow_1h, optimal_max_snow_1h, optimal_daytime) VALUES (10, walking, -5, 30, 15, 25, 5, 0, 40, 5, 30, 0, 0, 0, 0, 0, 0, 0, 0, False)");
+        db.execSQL("INSERT INTO " + TABLE_NAME_PA+ "(id, name, min_temp, max_temp, optimal_min_temp, optimal_max_temp, optimal_min_visibility, min_wind_speed, max_wind_speed, optimal_min_wind, optimal_max_wind, min_rain_1h, max_rain_1h, optimal_min_rain_1h, optimal_max_rain_1h, min_snow_1h, max_snow_1h, optimal_min_snow_1h, optimal_max_snow_1h, optimal_daytime) VALUES (20, running, 0, 25, 10, 20, 10, 0, 30, 5, 25, 0, 0, 0, 0, 0, 0, 0, 0, False)");
+        db.execSQL("INSERT INTO " + TABLE_NAME_PA+ "(id, name, min_temp, max_temp, optimal_min_temp, optimal_max_temp, optimal_min_visibility, min_wind_speed, max_wind_speed, optimal_min_wind, optimal_max_wind, min_rain_1h, max_rain_1h, optimal_min_rain_1h, optimal_max_rain_1h, min_snow_1h, max_snow_1h, optimal_min_snow_1h, optimal_max_snow_1h, optimal_daytime) VALUES (30, cycling, 0, 25, 5, 20, 15, 0, 30, 5, 25, 0, 0, 0, 0, 0, 0, 0, 0, True)");
+        db.execSQL("INSERT INTO " + TABLE_NAME_PA+ "(id, name, min_temp, max_temp, optimal_min_temp, optimal_max_temp, optimal_min_visibility, min_wind_speed, max_wind_speed, optimal_min_wind, optimal_max_wind, min_rain_1h, max_rain_1h, optimal_min_rain_1h, optimal_max_rain_1h, min_snow_1h, max_snow_1h, optimal_min_snow_1h, optimal_max_snow_1h, optimal_daytime) VALUES (40, hiking, 10, 30, 15, 25, 20, 0, 25, 5, 20, 0, 0, 0, 0, 0, 0, 0, 0, True)");
+
+
 
     }
 
@@ -49,6 +96,8 @@ public class StepAppOpenHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     // nothing to do here
     }
+
+
 
     /**
      * Utility function to load all records in the database
@@ -74,6 +123,7 @@ public class StepAppOpenHelper extends SQLiteOpenHelper {
 
         Log.d("STORED TIMESTAMPS: ", String.valueOf(dates));
     }
+
 
     /**
      * Utility function to delete all records from the data base
